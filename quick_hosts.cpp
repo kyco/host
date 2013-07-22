a972d107abae83d6b85e89e9fd87844721dc61d2
Kyluke McDougall <kyluke@kyco.co.za>
2013-07-22 19:49:33 +0200

    Made modifications to the menus and added a clear screen feature which makes output cleaner Changed the interface to match the windows version Added a first time setup feature for new users

--- quick_hosts.cpp
+++ quick_hosts.cpp
@@ -65,6 +65,8 @@ string network_check();
 
 // Misc
 void ClearScreen();
+void no_profiles();
+int first_run();
 
 // Start of program
 int main(int argc, char const *argv[])
@@ -73,6 +75,12 @@ int main(int argc, char const *argv[])
     string argument; // Holds the variable passed to the program
     int quick_incr = 0; // Used to increment within the for loop. Required to store argument as it is switch + 1
 
+    if (first_run() == 0)
+    {
+        ClearScreen();
+        setup("1");
+    }
+    
     // Get the user ID
     if (getuid() != 0)
     {
@@ -124,9 +132,11 @@ void arguments(string ts, string a, int test) // Handles which function is calle
 void menu() // Function to display the menu
 {
     int option; // Stores the input of the user
+    
+    ClearScreen(); // Clear screen initially
 
-    cout << "===========================================================" << endl;
-    cout << "Options: " << endl;
+    cout << "Main Menu" << endl;
+    cout << "-----------------------------------------------------------" << endl << endl;
     cout << "1. Switch profile" << endl;
     cout << "2. Read hosts file" << endl;
     cout << "3. View profiles" << endl;
@@ -135,7 +145,7 @@ void menu() // Function to display the menu
     cout << "6. Check current network" << endl;
     cout << "9. Setup" << endl;
     cout << "0. Exit" << endl;
-    cout << "Option: ";
+    cout  << endl << "Option: ";
     cin >> option; // Gets the input of the user
     menu_option(option); // Calls the function which will direct to the user defined function
 }
@@ -144,35 +154,46 @@ void menu_option(int option) // Once a choice has een made, this function will c
 {
     switch(option) {
         case 1:
+            ClearScreen();
             choose_profile(); // Change active profile
             wait_for_menu(); // Waits for user input before displaying the menu again
             break;
         case 2:
+            ClearScreen();
             cout << view_hosts(); // Display contents of hosts file
             wait_for_menu();
             break;
         case 3:
+            ClearScreen();
             view_profiles(); // View the available profiles
             wait_for_menu();
             break;
         case 4:
+            ClearScreen();
             add_profile("1"); // Add new profile
             wait_for_menu();
             break;
         case 5:
+            ClearScreen();
             remove_profile("1"); // Remove a profile
             wait_for_menu();
             break;
         case 6:
+            ClearScreen();
             network_check(); // Remove a profile
             wait_for_menu();
             break;
         case 9:
+            ClearScreen();
             setup("1"); // Setup the software config files
             wait_for_menu();
             break;
         case 0:
+            ClearScreen();
             exit(0);
+        default:
+            ClearScreen();
+            menu();
     }
 }
 
@@ -333,6 +354,9 @@ void choose_profile() // Choose the profile
     int host = 0;
     string get;
 
+    cout << "Profile Switching" << endl;
+    cout << "-----------------------------------------------------------" << endl << endl;
+
     DIR *dir;
     struct dirent *ent;
     dir = opendir ("/etc/qs/profiles");
@@ -351,18 +375,23 @@ void choose_profile() // Choose the profile
                 control++;
             }
         }
+        if (control == 0)
+        {
+            no_profiles();
+        }
+
         for (int i = 0; i < control; ++i)
         {
             cout << i << ". " << contents[i] << endl;
         }
-        cout << "Option: ";
+        cout << endl << "Option: ";
         cin >> host;
         closedir (dir);
         switch_profile(contents[host].c_str());
     }
     else
     {
-        setup("");
+        setup("1");
     }
 }
 
@@ -371,6 +400,9 @@ void add_profile(string add_profile)
     string profile;
     string p_name;
     string view_line;
+    
+    cout << "Add profile" << endl;
+    cout << "-----------------------------------------------------------" << endl << endl;
 
     cout << "Please enter a name for your new profile: " << endl;
     cin >> p_name;
@@ -412,6 +444,9 @@ void remove_profile(string r_profile)
     int option;
     int chosen;
     string control_unit;
+    
+    cout << "Profile removal" << endl;
+    cout << "-----------------------------------------------------------" << endl << endl;
 
     if (r_profile == "1")
     {
@@ -420,9 +455,10 @@ void remove_profile(string r_profile)
         cout << "Option: ";
         cin >> option;
         cout << "\n";
+        ClearScreen();
         if (option == 2)
         {
-            cout << "Please pick a profile to remove" << endl;
+            cout << "Please pick a profile to remove" << endl << endl;
             DIR *dir;
             struct dirent *ent;
             dir = opendir ("/etc/qs/profiles");
@@ -436,18 +472,28 @@ void remove_profile(string r_profile)
                     control++;
                 }
             }
+            if (control == 0)
+            {
+                no_profiles();
+            }
+
             for (int i = 0; i < control; ++i)
             {
                 cout << i << ". " << contents[i] << endl;
             }
-            cout << "Option: ";
+            cout << endl << "Option: ";
             cin >> chosen;
             chdir("/etc/qs/profiles");
             remove_this_one = contents[chosen];
             if (remove(remove_this_one.c_str()) != 0)
             {
+                ClearScreen();
                 cout << "Remove failed. Please try again" << endl;
                 chdir("..");
+                std::cin.ignore(1024, '\n');
+                std::cout << "press enter to continue ";
+                std::cin.get();
+                ClearScreen();
                 remove_profile("1");
             }
             else
@@ -469,6 +515,7 @@ void remove_profile(string r_profile)
             }
             else
             {
+                ClearScreen();
                 cout << "Remove successful" << endl;
                 chdir("..");
             }
@@ -495,6 +542,7 @@ void remove_profile(string r_profile)
     std::cin.ignore(1024, '\n');
     std::cout << "press enter to continue ";
     std::cin.get();
+    ClearScreen();
     menu();
 }
 
@@ -504,6 +552,9 @@ void view_profiles()
     string contents[50];
     int control = 0;
     string control_unit;
+    
+    cout << "Available profiles" << endl;
+    cout << "-----------------------------------------------------------" << endl << endl;
 
     DIR *dir;
     struct dirent *ent;
@@ -521,13 +572,14 @@ void view_profiles()
     }
     if (control == 0)
     {
-        cout << "You currently have no profiles" << endl;
+        no_profiles();
     }
     for (int i = 0; i < control; ++i)
     {
         cout << i << ". " << contents[i] << endl;
     }
     chdir("..");
+    cout << endl;
 }
 
 /* Detects the network address
@@ -572,4 +624,54 @@ string network_check()
 void ClearScreen()
 {
 	cout << "\033[2J\033[1;1H";
+}  
+    
+int first_run()
+{
+    const char* pzPath;
+    pzPath = "/etc/qs";
+    if ( pzPath == NULL) return false;
+
+    DIR *pDir;
+    bool bExists = 0;
+
+    pDir = opendir (pzPath);
+
+    if (pDir != NULL)
+    {
+        bExists = 1;    
+        (void) closedir (pDir);
+    }
+
+    return bExists;
+}
+
+
+void no_profiles()
+{
+    int add_new_profile = 0;
+    
+    cout << "You currently have no profiles, would you like to add one?" << endl;
+    cout << "0. Yes" << endl;
+    cout << "1. No" << endl;
+    do
+    {
+         add_new_profile = 0;
+         cout << endl << "Option: ";
+         cin >> add_new_profile;
+    }
+    while (add_new_profile > 1);
+    
+    if (add_new_profile == 0)
+    {
+        ClearScreen();
+        add_profile("1");
+        wait_for_menu();
+        menu();
+    }
+    else if (add_new_profile == 1)
+    {
+         ClearScreen();
+         menu();
+    }
 }
